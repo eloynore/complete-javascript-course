ca49fe5b572e015f4047f313482d19a1c71e3c4e
eloynore <54298782+eloynore@users.noreply.github.com>
2023-02-07 20:16:59 +0100

    data types lecture and exercises added

--- 01-Fundamentals-Part-1/starter/assigments.js
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -1,4 +1,4 @@
-// Exercise 1 LESSON 10
+/*// Exercise 1 LESSON 10
 let country = 'spain';
 let continent = 'europe';
 let population = 46000000;
@@ -7,3 +7,17 @@ let population = 46000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+
+// Exercise 1 LESSON DATA TYPES
+let isIsland = false;
+let population = 2;
+let country = "Spain";
+let languaje;
+
+// Exercise 2 LESSON DATA TYPES
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,5 @@
-let js = 'amazing';
-console.log(40+8+23-10);
+/*let js = 'amazing';
+// console.log(40+8+23-10);
 console.log('Jonas');
 
 let firstName = 'Matilda'; // cammel convention
@@ -17,3 +17,25 @@ let myFristJob = "Programmer";
 let myCurrentJob = "Astronaut"; // your variable name should be a description of the value
 
 console.log(myFristJob);
+*/
+
+true;
+console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun); // type of the variable that we have 
+console.log(typeof 23);
+console.log(typeof 'javascript');
+
+javascriptIsFun = "jaja!"; // changing the variable value 
+console.log(typeof javascriptIsFun); // checking the type after modifying
+
+let year; // undefined its both the value and the type of the value
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+
+console.log(typeof null); // returns object
