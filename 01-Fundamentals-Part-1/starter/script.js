333054b62e30d751adf05ea3eff8a876c6736a54
eloynore <54298782+eloynore@users.noreply.github.com>
2023-02-07 19:48:58 +0100

    lesson 10 variable naming

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing') {
-    alert('JavaScript is FUN!')
-}
 console.log(40+8+23-10);
-//test change for yo que se
+console.log('Jonas');
+
+let firstName = 'Matilda'; // cammel convention
+let first = "Juan";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM"; // underscore to separate words 
+let $function = 27;
+
+let PI= 3.14 // constants will be named in UPPERCASE 
+let myFristJob = "Programmer";
+let myCurrentJob = "Astronaut"; // your variable name should be a description of the value
+
+console.log(myFristJob);
