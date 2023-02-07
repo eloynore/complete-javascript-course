d9ddc7798e837b21925bb2a8c2cc685a0216bc58
eloynore <54298782+eloynore@users.noreply.github.com>
2023-02-07 19:55:38 +0100

    lesson 10 exercises added

---
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -0,0 +1,9 @@
+// Exercise 1 LESSON 10
+let country = 'spain';
+let continent = 'europe';
+let population = 46000000;
+
+// Exercise 2 LESSON 10
+console.log(country);
+console.log(continent);
+console.log(population);
