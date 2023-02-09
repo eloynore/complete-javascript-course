1c624739557582f59a46b28518bd51ab63a8afdb
eloynore <54298782+eloynore@users.noreply.github.com>
2023-02-09 19:47:52 +0100

    string exercise complete

--- 01-Fundamentals-Part-1/starter/assigments.js
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -20,7 +20,7 @@ let languaje;
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
-console.log(typeof population)*/
+console.log(typeof population)
 
 // Exercise 1 2 3  LET CONSTANT VAR
 const isIsland = false;
@@ -28,4 +28,18 @@ let population = 2;
 const country = "Spain";
 let languaje;
 
-languaje = "spanish";
+languaje = "spanish";*/
+
+// Exercise strings and template literals
+let description = `
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+`
