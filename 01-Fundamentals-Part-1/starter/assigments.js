68fec6dd02cfde1dc5a87465453f2ce92dca9fe7
eloynore <54298782+eloynore@users.noreply.github.com>
2023-02-13 20:58:21 +0100

    add the switch statement exercises

--- 01-Fundamentals-Part-1/starter/assigments.js
+++ 01-Fundamentals-Part-1/starter/assigments.js
@@ -54,12 +54,36 @@ people speak portuguese'
 // }
 
 // EQUALITY OPERATORS
-const numNeighbours = Number(prompt('how many neighbour countries does your country have?'))
+// const numNeighbours = Number(prompt('how many neighbour countries does your country have?'))
 
-if (numNeighbours === 1){
-    console.log('Only 1 border');
-}else if( numNeighbours >= 1){
-    console.log('More than 1 border');
-}else{
-    console.log('No borders')
+// if (numNeighbours === 1){
+//     console.log('Only 1 border');
+// }else if( numNeighbours >= 1){
+//     console.log('More than 1 border');
+// }else{
+//     console.log('No borders')
+// }
+
+// THE SWITCH STATEMENT
+const language = prompt("LANGUAJE");
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!')
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers')
+        break;
+    case 'english':
+        console.log('3rd place')
+        break;
+    case 'hindi':
+        console.log('Number 4')
+        break;
+    case 'arabic':
+        console.log('5th most spoken language')
+        break;
+    default:
+        console.log('Great language too :D')
+        break;
 }
